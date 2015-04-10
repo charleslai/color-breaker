30c6fe3cc253644be6ffb7b71c82e2d8f72c9b96
Charles Lai <cjl223@cornell.edu>
2015-04-10 12:47:30 -0400

    init commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+=============
+color-breaker
+=============
+
+lol
+
+============
+Installation
+============
+* Download this as a zip file to the desktop and unpack it.
+* Navigate to chrome://extensions/ in Chrome and select developer mode.
+* Click on "Load unpacked extension..." and choose the color-breaker directory.
---
+++ color-breaker.js
@@ -0,0 +1,33 @@
+/*
+  unfeed.js
+  Removes key profile information from the newsfeed
+  @author: Charles J Lai
+*/
+
+//Constants
+var UPDATE_INTERVAL = 100;
+var LONG_UPDATE_INTERVAL = 10000;
+
+//Main color clicking function
+function clickColors() {
+  //First create an event
+  var click_ev = document.createEvent("MouseEvents");
+  // initialize the event
+  click_ev.initEvent("click", true /* bubble */, true /* cancelable */);
+  //Get arrays of nodes
+  var colorboxes = document.getElementById("box").childNodes;
+  for (var i = 0; i < colorboxes.length; i++){
+    //trigger the evevnt
+    colorboxes[i].dispatchEvent(click_ev);
+  }
+}
+
+///////////////
+//MAIN SCRIPT//
+///////////////
+// Initialize the click event
+
+//Initial run of the script
+clickColors();
+//Attempt to remove more information on UPDATE_INTERVAL
+setInterval(clickColors, UPDATE_INTERVAL);
